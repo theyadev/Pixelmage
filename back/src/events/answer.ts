3fabdde62b5a7ce0b83dfe0dccbba8bc8185e456
Fassi Théo <gtheya@gmail.com>
2021-11-17 15:54:07 +0100

    changed point system
    Co-authored-by: fmaret <fmaret@users.noreply.github.com>

--- back/src/events/answer.ts
+++ back/src/events/answer.ts
@@ -19,16 +19,25 @@ export default function Answer(
 
     const currentRoundImageIndex = room.currentRound - 1;
     if (
-      data.answer.toLowerCase() == room.answers[currentRoundImageIndex].answer.toLowerCase() ||
-      room.answers[currentRoundImageIndex].aliases?.some(e => e.toLowerCase() == data.answer.toLowerCase())
+      data.answer.toLowerCase() ==
+        room.answers[currentRoundImageIndex].answer.toLowerCase() ||
+      room.answers[currentRoundImageIndex].aliases?.some(
+        (e) => e.toLowerCase() == data.answer.toLowerCase()
+      )
     ) {
       const index = getUserIndex(room, data.id, data.name);
 
-      if (index === undefined) return;
+      if (index === -1) return;
 
       room.users[index].answered = true;
 
-      room.users[index].score++;
+      const max = 1000;
+      const min = 100;
+
+      room.users[index].score +=
+        Math.floor(
+          ((1 - room.currentTime / room.maxTime) * (max - min) + min) / 10
+        ) * 10;
 
       socket.emit("GOOD ANSWER");
 
