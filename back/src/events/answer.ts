f486537b1052bb53abee9e50e4291f159aebf2ca
fmaret <maret.florent59@gmail.com>
2021-11-18 00:12:26 +0100

    levenshtein (doesn't work for now)

--- back/src/events/answer.ts
+++ back/src/events/answer.ts
@@ -4,6 +4,29 @@ import { Room } from "../types";
 import update from "../functions/update";
 import getUserIndex from "../functions/getUserIndex";
 
+
+export function levenshteinDistance(str1 = '', str2 = ''){
+  const track = Array(str2.length + 1).fill(null).map(() =>
+  Array(str1.length + 1).fill(null));
+  for (let i = 0; i <= str1.length; i += 1) {
+     track[0][i] = i;
+  }
+  for (let j = 0; j <= str2.length; j += 1) {
+     track[j][0] = j;
+  }
+  for (let j = 1; j <= str2.length; j += 1) {
+     for (let i = 1; i <= str1.length; i += 1) {
+        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        track[j][i] = Math.min(
+           track[j][i - 1] + 1, // deletion
+           track[j - 1][i] + 1, // insertion
+           track[j - 1][i - 1] + indicator, // substitution
+        );
+     }
+  }
+  return (str1.length+str2.length - track[str2.length][str1.length])/(str1.length+str2.length);
+};
+
 export default function Answer(
   io: any,
   socket: Socket,
@@ -15,14 +38,19 @@ export default function Answer(
   socket.on("ANSWER", function (data) {
     const room = Rooms.get(data.id);
 
+    const percentAccepted = 0.5;
+    const percentNear = 0.8;
+
     if (!room) return;
 
     const currentRoundImageIndex = room.currentRound - 1;
+
+
     if (
-      data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase() ==
-        room.answers[currentRoundImageIndex].answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase() ||
+      levenshteinDistance(data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(),
+        room.answers[currentRoundImageIndex].answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentAccepted ||
       room.answers[currentRoundImageIndex].aliases?.some(
-        (e) => e.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase() == data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
+        (e) => levenshteinDistance(e.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(), data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentAccepted
       )
     ) {
       const index = getUserIndex(room, data.id, data.name);
@@ -42,8 +70,19 @@ export default function Answer(
       socket.emit("GOOD ANSWER");
 
       update(io, room);
+    }
+    else if (
+      levenshteinDistance(data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(),
+        room.answers[currentRoundImageIndex].answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentNear ||
+      room.answers[currentRoundImageIndex].aliases?.some(
+        (e) => levenshteinDistance(e.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(), data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentNear
+      )
+    ){
+      socket.emit("WRONG ANSWER"); // A CHANGER "NEAR ANSWER"
     } else {
       socket.emit("WRONG ANSWER");
     }
   });
 }
+
+
