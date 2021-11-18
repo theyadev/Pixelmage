31e3a5dd9bcd72de1a866bbf742a1fcc89ec13f2
theyadev <gtheya@gmail.com>
2021-11-18 10:25:09 +0100

    Added near answer possibility

--- back/src/events/answer.ts
+++ back/src/events/answer.ts
@@ -1,31 +1,44 @@
 import { Socket } from "socket.io";
-import { Room } from "../types";
+import { Answer, Room } from "../types";
 
 import update from "../functions/update";
 import getUserIndex from "../functions/getUserIndex";
 
+import levenshtein from "../functions/levensthein";
 
-export function levenshteinDistance(str1 = '', str2 = ''){
-  const track = Array(str2.length + 1).fill(null).map(() =>
-  Array(str1.length + 1).fill(null));
-  for (let i = 0; i <= str1.length; i += 1) {
-     track[0][i] = i;
-  }
-  for (let j = 0; j <= str2.length; j += 1) {
-     track[j][0] = j;
-  }
-  for (let j = 1; j <= str2.length; j += 1) {
-     for (let i = 1; i <= str1.length; i += 1) {
-        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
-        track[j][i] = Math.min(
-           track[j][i - 1] + 1, // deletion
-           track[j - 1][i] + 1, // insertion
-           track[j - 1][i - 1] + indicator, // substitution
-        );
-     }
-  }
-  return (str1.length+str2.length - track[str2.length][str1.length])/(str1.length+str2.length);
-};
+function completeNormalize(str: string) {
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+}
+
+function checkAnswer(
+  userAnswer: string,
+  roomAnswer: Answer,
+  minPercentage: number
+) {
+  return (
+    levenshtein(
+      completeNormalize(userAnswer),
+      completeNormalize(roomAnswer.answer)
+    ) >= minPercentage ||
+    roomAnswer.aliases?.some(
+      (e) =>
+        levenshtein(completeNormalize(e), completeNormalize(userAnswer)) >=
+        minPercentage
+    )
+  );
+}
+
+function calculateScore(currentTime: number, maxTime: number) {
+  const max = 1000;
+  const min = 100;
+
+  return (
+    Math.floor(((1 - currentTime / maxTime) * (max - min) + min) / 10) * 10
+  );
+}
 
 export default function Answer(
   io: any,
@@ -38,51 +51,29 @@ export default function Answer(
   socket.on("ANSWER", function (data) {
     const room = Rooms.get(data.id);
 
-    const percentAccepted = 0.5;
-    const percentNear = 0.8;
+    const percentAccepted = 80;
+    const percentNear = 65;
 
     if (!room) return;
 
-    const currentRoundImageIndex = room.currentRound - 1;
+    const currentRoundAnswer = room.answers[room.currentRound - 1];
 
-
-    if (
-      levenshteinDistance(data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(),
-        room.answers[currentRoundImageIndex].answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentAccepted ||
-      room.answers[currentRoundImageIndex].aliases?.some(
-        (e) => levenshteinDistance(e.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(), data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentAccepted
-      )
-    ) {
+    if (checkAnswer(data.answer, currentRoundAnswer, percentAccepted)) {
       const index = getUserIndex(room, data.id, data.name);
 
       if (index === -1) return;
 
       room.users[index].answered = true;
 
-      const max = 1000;
-      const min = 100;
-
-      room.users[index].score +=
-        Math.floor(
-          ((1 - room.currentTime / room.maxTime) * (max - min) + min) / 10
-        ) * 10;
+      room.users[index].score += calculateScore(room.currentTime, room.maxTime);
 
       socket.emit("GOOD ANSWER");
 
       update(io, room);
-    }
-    else if (
-      levenshteinDistance(data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(),
-        room.answers[currentRoundImageIndex].answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentNear ||
-      room.answers[currentRoundImageIndex].aliases?.some(
-        (e) => levenshteinDistance(e.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(), data.answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()) >= percentNear
-      )
-    ){
-      socket.emit("WRONG ANSWER"); // A CHANGER "NEAR ANSWER"
+    } else if (checkAnswer(data.answer, currentRoundAnswer, percentNear)) {
+      socket.emit("NEAR ANSWER");
     } else {
       socket.emit("WRONG ANSWER");
     }
   });
 }
-
-
