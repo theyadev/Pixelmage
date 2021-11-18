7091ed12fc2a0c22251d754fd7e563dca063b9e9
Fassi Théo <gtheya@gmail.com>
2021-11-18 20:46:26 +0100

    changed server url

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -7,8 +7,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     username: null,
-    socket: io("http://localhost:8000", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
-    // socket: io("http://93.6.41.243:8271/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
+    // socket: io("http://localhost:8000", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
+    socket: io("http://93.6.41.243:8271/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
     connected: false,
   },
   mutations: {
