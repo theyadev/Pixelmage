250e244adbb585da3c61abac26efd497932c0f35
theyadev <gtheya@gmail.com>
2021-11-19 11:42:57 +0100

    use bug url

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -8,7 +8,8 @@ export default new Vuex.Store({
   state: {
     username: null,
     // socket: io("http://localhost:8000", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
-    socket: io("http://93.6.41.243:8271/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
+    // socket: io("http://93.6.41.243:8271/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
+    socket: io("http://api.pixelmage.tk/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
     connected: false,
   },
   mutations: {
