0c6d15c401460c5c200fb7c2fac56cd82e4530df
Fassi Théo <gtheya@gmail.com>
2021-11-24 21:19:17 +0100

    changed server url

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -7,9 +7,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     username: null,
+    socket: io("pixelmage-back-f46xp.ondigitalocean.app", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
     // socket: io("http://localhost:8000", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
-    socket: io("http://93.6.41.243:8271/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
-    // socket: io("http://api.pixelmage.tk/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
+    // socket: io("http://93.6.41.243:8271/", {transports: ['websocket'], upgrade: false, closeOnBeforeunload: false}),
     connected: false,
   },
   mutations: {
