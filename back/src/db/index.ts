a5dfe454fd0852a13aeb65d7d7fbd5c8a3656822
Fassi Théo <gtheya@gmail.com>
2021-11-28 21:19:43 +0100

    added cache
    Co-authored-by: fmaret <fmaret@users.noreply.github.com>

--- back/src/db/index.ts
+++ back/src/db/index.ts
@@ -1,8 +1,17 @@
 import Mongoose from "mongoose";
 
 import { config } from "dotenv";
+import { Answer, Category } from "../types";
 
 config();
+
+type Cache = {
+  date?: Date;
+  images?: Answer[];
+  categories?: Category[];
+}
+
+let cache: Cache = {};
 
 let database: Mongoose.Connection;
 
@@ -25,26 +34,46 @@ function getRandomItems(list: any[], nb: number) {
   return randomSorted.splice(0, nb);
 }
 
+function isCacheRecent() {
+  return cache.date && cache.date >= new Date(new Date().getTime() - 10*60000)
+}
+
 export async function getCategories() {
+  if (cache.categories && isCacheRecent()) {
+    return cache.categories
+  }
+
   await connect();
 
   const categoriesDatabase = database.collection("categories");
 
-  return await categoriesDatabase.find({}).toArray()
+  const res : any =  await categoriesDatabase.find({}).toArray()
+  const categories : Category[] = res
+
+  cache.categories = categories
+  cache.date = new Date()
+
+  return categories
 }
 
 async function getCategoriesIds(categoriesNames: string[]) {
-  const categoriesDatabase = database.collection("categories");
+  let categories: Category[];
 
-  const categories = await categoriesDatabase
-    .find({
-      category: {
-        $in: categoriesNames,
-      },
-    })
-    .toArray();
+  if (cache.categories && isCacheRecent()) {
+    categories = cache.categories
+  } else {
+    await connect();
 
-  const categoriesIds: number[] = categories.map((e) => {
+    const categoriesDatabase = database.collection("categories");
+
+    const res: any = await categoriesDatabase.find({}).toArray();
+
+    categories = res
+  }
+
+  const filteredCategories = categories.filter(e => categoriesNames.includes(e.category))
+
+  const categoriesIds: number[] = filteredCategories.map((e) => {
     return e.id;
   });
 
@@ -52,19 +81,21 @@ async function getCategoriesIds(categoriesNames: string[]) {
 }
 
 export async function getImages(categoriesFilters: string[], nb: number) {
-  await connect();
+  let images: Answer[];
 
-  const imagesDatabase = database.collection("images");
+  if (cache.images && isCacheRecent()) {
+    images = cache.images
+  } else {
+    await connect();
+
+    const imagesDatabase = database.collection("images");
+    const res: any = await imagesDatabase.find({}).toArray();
+    images = res
+  }
 
   const categories = await getCategoriesIds(categoriesFilters);
 
-  const images = await imagesDatabase
-    .find({
-      categoryId: {
-        $in: categories,
-      },
-    })
-    .toArray();
+  images = images.filter(e => categories.includes(e.categoryId))
 
   return getRandomItems(images, nb);
 }
